166a273875bfce0b666a0aca564745039fbd90ac
khushaboopandey <45198458+khushaboopandey@users.noreply.github.com>
2024-07-25 13:52:53 +0530

    netlify changes

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,8 +2,7 @@ import { useState, useEffect } from "react";
 
 const useFetch = (apiPath, queryTerms = "") => {
   const [data, setData] = useState([]);
-  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerms}
-`;
+  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerms}`;
 
   useEffect(() => {
     async function fetchMovie() {
