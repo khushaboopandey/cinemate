0a48b3dc0dc1e8ad447d5a302f32e1800afd043d
khushaboopandey <45198458+khushaboopandey@users.noreply.github.com>
2024-07-27 20:57:50 +0530

    fixed few errors upon serach

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -28,44 +28,46 @@ export const MovieList = ({ apiPath, title }) => {
           ))}
         </div>
         {/* Pagination */}
-        <div className="m-auto text-center py-5">
-          <nav aria-label="Page navigation example">
-            <ul className="inline-flex -space-x-px text-sm">
-              <li>
-                <button
-                  onClick={() => handlePageChange(currentPage - 1)}
-                  disabled={currentPage === 1}
-                  className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  Previous
-                </button>
-              </li>
-              {[...Array(5)].map((_, index) => (
-                <li key={index}>
+        {movies?.length > 0 && (
+          <div className="m-auto text-center py-5">
+            <nav aria-label="Page navigation example">
+              <ul className="inline-flex -space-x-px text-sm">
+                <li>
                   <button
-                    onClick={() => handlePageChange(index + 1)}
-                    className={`flex items-center justify-center px-3 h-8 leading-tight ${
-                      currentPage === index + 1
-                        ? "text-blue-600 border border-gray-300 bg-blue-50 dark:bg-gray-700 dark:text-white"
-                        : "text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                    }`}
+                    onClick={() => handlePageChange(currentPage - 1)}
+                    disabled={currentPage === 1}
+                    className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                   >
-                    {index + 1}
+                    Previous
                   </button>
                 </li>
-              ))}
-              <li>
-                <button
-                  onClick={() => handlePageChange(currentPage + 1)}
-                  disabled={currentPage === 5} // Assuming 5 pages for demonstration
-                  className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  Next
-                </button>
-              </li>
-            </ul>
-          </nav>
-        </div>
+                {[...Array(5)].map((_, index) => (
+                  <li key={index}>
+                    <button
+                      onClick={() => handlePageChange(index + 1)}
+                      className={`flex items-center justify-center px-3 h-8 leading-tight ${
+                        currentPage === index + 1
+                          ? "text-blue-600 border border-gray-300 bg-blue-50 dark:bg-gray-700 dark:text-white"
+                          : "text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                      }`}
+                    >
+                      {index + 1}
+                    </button>
+                  </li>
+                ))}
+                <li>
+                  <button
+                    onClick={() => handlePageChange(currentPage + 1)}
+                    disabled={currentPage === 5} // Assuming 5 pages for demonstration
+                    className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    Next
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        )}
       </section>
     </main>
   );
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -28,7 +28,20 @@ export const Search = ({ apiPath, title }) => {
             <Card key={movie.id} movie={movie} />
           ))}
         </div>
-        <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} />
+        {movies.length > 0 && (
+          <Pagination
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          />
+        )}
+
+        {movies.length === 0 && (
+          <div className="flex flex-col justify-center items-center h-[63vh] text-gray-800 dark:text-gray-300">
+            <p className="text-3xl px-24 text-gray-700 dark:text-white">
+              Please Search a valid Movie
+            </p>
+          </div>
+        )}
       </section>
     </main>
   );
