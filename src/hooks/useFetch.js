1744d6314a630a64f1ad3a26d33ab4be17ae53b8
khushaboopandey <45198458+khushaboopandey@users.noreply.github.com>
2024-07-27 17:12:08 +0530

    added Padination

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 
-const useFetch = (apiPath, queryTerms = "") => {
+const useFetch = (apiPath, queryTerms = "", page = 1) => {
   const [data, setData] = useState([]);
-  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerms}`;
+  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerms}&page=${page}`;
 
   useEffect(() => {
     async function fetchMovie() {
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,11 +1,17 @@
+import React, { useState } from "react";
 import { Card } from "../component/Card";
 import useFetch from "../hooks/useFetch";
 import useTitle from "../hooks/useTitle";
 
 export const MovieList = ({ apiPath, title }) => {
-  const { data: movies } = useFetch(apiPath);
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data: movies } = useFetch(apiPath, "", currentPage);
 
   useTitle(title);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
   return (
     <main>
@@ -14,6 +20,45 @@ export const MovieList = ({ apiPath, title }) => {
           {movies.map((movie) => (
             <Card key={movie.id} movie={movie} />
           ))}
+        </div>
+        {/* TODO Move Pagination code to its seperate comp*/}
+        <div className="m-auto text-center py-5">
+          <nav aria-label="Page navigation example">
+            <ul className="inline-flex -space-x-px text-sm">
+              <li>
+                <button
+                  onClick={() => handlePageChange(currentPage - 1)}
+                  disabled={currentPage === 1}
+                  className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  Previous
+                </button>
+              </li>
+              {[...Array(5)].map((_, index) => (
+                <li key={index}>
+                  <button
+                    onClick={() => handlePageChange(index + 1)}
+                    className={`flex items-center justify-center px-3 h-8 leading-tight ${
+                      currentPage === index + 1
+                        ? "text-blue-600 border border-gray-300 bg-blue-50 dark:bg-gray-700 dark:text-white"
+                        : "text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    }`}
+                  >
+                    {index + 1}
+                  </button>
+                </li>
+              ))}
+              <li>
+                <button
+                  onClick={() => handlePageChange(currentPage + 1)}
+                  disabled={currentPage === 5} // Assuming 5 pages for demonstration
+                  className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  Next
+                </button>
+              </li>
+            </ul>
+          </nav>
         </div>
       </section>
     </main>
